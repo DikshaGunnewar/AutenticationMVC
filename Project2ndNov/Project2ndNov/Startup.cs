7fab4e98f2b05cf78d9500671f28ae53557444cf
dikshagunnewargit <diksha1995gunnewar@gmail.com>
2017-11-02 17:11:48 +0530

    Inital Commit MVC Authentication Demos RoleBased1 Main

---
+++ Project2ndNov/Project2ndNov/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project2ndNov.Startup))]
+namespace Project2ndNov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
