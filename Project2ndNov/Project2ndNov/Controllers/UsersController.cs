538117dd8f683c6239bd2bfc33bf0550a033d72b
dikshagunnewargit <diksha1995gunnewar@gmail.com>
2017-11-02 17:35:13 +0530

    Demo Sample projects Commit 2 For reference only

---
+++ Project2ndNov/Project2ndNov/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Project2ndNov.CustomFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2ndNov.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: User
+        [AuthLog(Roles = "User")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
